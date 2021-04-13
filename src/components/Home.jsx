7995a0241672f726fbf2ab25d02d61259df48009
Luzciel <luzcielmontesinos@gmail.com>
2021-04-12 23:27:19 -0400

    archivo Json reduccion de la descripcion del producto

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment, useState } from 'react'
+
+
+
+
+const MadeForYou = () => {
+
+
+
+    return (
+        <Fragment>
+            <h2>Home en construccion</h2>
+            <a href="/madeForYou">Hecho para Ti</a>
+        </Fragment>
+
+
+    )
+}
+
+export default MadeForYou;
