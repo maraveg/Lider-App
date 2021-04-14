b4c6676489327b1c16d04dcae219ee6fd7ca61fe
maraveg <taravenaguerra@gmail.com>
2021-04-14 08:28:34 -0400

    Se modificó el home, se añadió modal y se crearon los estilos de home, pendiente modificar css de modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,51 @@
 import {Link} from "react-router-dom";
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import './Home.css'; 
-import Gif from './ProductosPasando.gif'
+import Gif from './productosPasando.gif'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
 const Home = () =>{
+	const [modalIsOpen, setModalIsOpen] = useState(false)
+	useEffect(()=> {
+		setTimeout(function(){setModalIsOpen(true)}, 2000)
+
+	}, [])
 	return (
 		<section id="background">
-			<div id="banner-container">
-				<div id="image-container">
-					<img id="gif-img" src={Gif} alt="g"/>
-				</div>
-				<div id="text-button">
-					<p>Juana, encuentra tus favoritos a un solo click</p>
-					<Link to="/madeForYou"><button id="enter-btn">Click</button></Link>
+			<div id="banner-divcontainer">
+				<div id="banner-container">
+					<div id="image-container">
+						<img id="gif-img" src={Gif} alt="g"/>
+					</div>
+					<div id="text-button">
+						<p>Juana, encuentra tus favoritos a un solo click</p>
+						<Link to="/madeForYou"><button id="enter-btn">Click</button></Link>
+					</div>
 				</div>
 			</div>
+			 <div className='App'>
+      {/* <button onClick={() => setModalIsOpen(true)}>Open modal</button> */}
+      <Modal
+        isOpen={modalIsOpen}
+				shouldCloseOnOverlayClick={false}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            backgroundColor: 'grey'
+          },
+          content: {
+            color: 'orange'
+          }
+        }}
+      >
+        <h2>Modal title</h2>
+        <div>Modal Body</div>
+        <div>
+          <button onClick={() => setModalIsOpen(false)}>Close</button>
+        </div>
+      </Modal>
+    </div>
 		</section>
 	);
   }
